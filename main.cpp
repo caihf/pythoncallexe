93241c6fb547a420ff861848a9c84872dd330886
name <haifeng.cai@youhualin.com>
2017-08-17 11:51:55 +0800

    add testfile

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: main.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2017年08月17日 星期四 11时43分18秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    std::cerr<<"hash = "<<GIT_HASH<<", time = "<<COMPILE_TIME<<", branch = "<<GIT_BRANCH<<std::endl;
+}
---
+++ makefile
@@ -0,0 +1,7 @@
+GIT_HASH=`git -rev-parse HEAD`
+COMPILE_TIME=`date -u +'%Y-%m-%d %H:%M:%S UTC'`
+GIT_BRANCH=`git branch | grep "^\*" | sed 's/^..//'`
+export VERSION_FLAGS=-DGIT_HASH="\"$(GIT_HASH)\"" -DCOMPILE_TIME="\"$(COMPILE_TIME)\"" -DGIT_BRANCH="\"$(GIT_BRANCH)\""
+
+all:
+	g++ main.cpp $(VERSION_FLAGS)
